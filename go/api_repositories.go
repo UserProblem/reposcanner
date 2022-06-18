ad7ccdcd75acc667eaca3582efd8d490a87a6331
Sean Critica <sean.critica@gmail.com>
2022-06-18 09:05:35 +0800

    Adding initial go.mod and fixing duplicate generated code.

--- go/api_repositories.go
+++ go/api_repositories.go
@@ -19,11 +19,6 @@ func AddRepository(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func AddScan(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-}
-
 func DeleteRepository(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)

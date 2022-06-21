acd608056ea55871c2774d8fea32d2bb802e7d95
Sean Critica <sean.critica@gmail.com>
2022-06-21 20:30:54 +0800

    Added implementation for the scan engine API.

---
+++ engine/controller.go
@@ -0,0 +1,123 @@
+package engine
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"log"
+
+	sw "github.com/UserProblem/reposcanner/go"
+)
+
+type Controller struct {
+	Incoming       chan *Job
+	Cancelling     chan *Job
+	Quit           chan bool
+	initializeFlag bool
+	QuitFlag       bool
+	nextJobId      chan string
+	scanHandler    ScanHandler
+}
+
+type Job struct {
+	Id     string
+	Repo   *sw.RepositoryInfo
+	Result chan *[]sw.FindingsInfo
+}
+
+type ScanHandler interface {
+	StartScan(*Job)
+	StopScan(*Job)
+}
+
+// Setup the controller for use
+func (c *Controller) Initialize(scanner ScanHandler) {
+	c.Incoming = make(chan *Job)
+	c.Cancelling = make(chan *Job)
+	c.Quit = make(chan bool)
+	c.QuitFlag = false
+	c.nextJobId = make(chan string)
+	go c.generateJobIds()
+
+	c.scanHandler = scanner
+
+	c.initializeFlag = true
+}
+
+// Generator to auto-generate the next unique id value that
+// can be used for new jobs.
+func (c *Controller) generateJobIds() {
+	nextId := uint64(0)
+	for {
+		c.nextJobId <- encodeJobId(uint64(nextId))
+	}
+}
+
+// Helper function to convert a numeric value into a base64
+// string value that can be used as an id
+func encodeJobId(v uint64) string {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, v)
+	return base64.RawURLEncoding.EncodeToString(b)
+}
+
+// Starts the controller. It will continue running until a
+// message is received from the Quit channel. Initialize() must be
+// called before calling this function.
+func (c *Controller) Run() {
+	for !c.QuitFlag {
+		c.RunOnce()
+	}
+}
+
+// The controller will execute for one loop. Initialize() must be
+// called before calling this function.
+func (c *Controller) RunOnce() {
+	if !c.initializeFlag {
+		log.Fatalf("Controller started before initialization.\n")
+	}
+
+	select {
+	case job := <-c.Incoming:
+		log.Printf("Handling incoming job. Id: %v\n", job.Id)
+		go c.scanHandler.StartScan(job)
+	case job := <-c.Cancelling:
+		log.Printf("Handling cancellation of job. Id: %v\n", job.Id)
+		go c.scanHandler.StopScan(job)
+	case _ = <-c.Quit:
+		log.Println("Stopping controller.")
+		c.QuitFlag = true
+	}
+}
+
+// Notifies the controller to stop running. This is not guaranteed
+// to be immediate, and pending jobs may still be processed before
+// execution stops.
+func (c *Controller) Stop() {
+	go func() { c.Quit <- true }()
+}
+
+// API to allow users to add jobs to the controller queue. Returns a
+// Job struct containing the identifier for the queued job, as well as
+// the results channel where the output will be sent.
+func (c *Controller) AddJob(ri *sw.RepositoryInfo) *Job {
+	log.Printf("Received request to scan '%v'\n", ri.Name)
+
+	// TODO validate url
+
+	job := Job{
+		Id:     <-c.nextJobId,
+		Repo:   ri.Clone(),
+		Result: make(chan *[]sw.FindingsInfo),
+	}
+	go func() { c.Incoming <- &job }()
+	return &job
+}
+
+// API to allow users to cancel jobs that are in the controller queue.
+// When a job is cancelled by the user, the user should no longer perform
+// any additional processing for the job. No further data should be expected
+// from the results channel.
+func (c *Controller) RemoveJob(job *Job) {
+	log.Printf("Received request to cancel job '%v'\n", job.Id)
+	go func() { c.Cancelling <- job }()
+}
---
+++ engine/controller_test.go
@@ -0,0 +1,142 @@
+package engine_test
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/UserProblem/reposcanner/engine"
+	sw "github.com/UserProblem/reposcanner/go"
+)
+
+type DummyScanner struct {
+	Started chan *engine.Job
+	Stopped chan *engine.Job
+}
+
+func (o *DummyScanner) StartScan(j *engine.Job) {
+	log.Printf("Dummy scanner start received job %v\n", j.Id)
+	o.Started <- j
+}
+
+func (o *DummyScanner) StopScan(j *engine.Job) {
+	log.Printf("Dummy scanner stop received job %v\n", j.Id)
+	o.Stopped <- j
+}
+
+func initializeDummyScanner() *DummyScanner {
+	return &DummyScanner{
+		Started: make(chan *engine.Job),
+		Stopped: make(chan *engine.Job),
+	}
+}
+
+func setup() (*engine.Controller, *DummyScanner) {
+	var c engine.Controller
+
+	o := initializeDummyScanner()
+	c.Initialize(o)
+
+	return &c, o
+}
+
+func TestAddJobAddsToIncomingChannel(t *testing.T) {
+	c, _ := setup()
+
+	ri := sw.DefaultRepositoryInfo()
+	job := c.AddJob(ri)
+	if job == nil {
+		t.Fatalf("Could not add job to the queue.")
+	}
+
+	timeout := time.NewTimer(1 * time.Second)
+
+	select {
+	case queuedJob := <-c.Incoming:
+		if job.Id != queuedJob.Id {
+			t.Errorf("Job expected id %v. Got %v\n", job.Id, queuedJob.Id)
+		}
+	case <-timeout.C:
+		t.Errorf("Job was not added to the queue.\n")
+	}
+}
+
+func TestRunOnceHandlesJobFromIncomingChannel(t *testing.T) {
+	c, o := setup()
+
+	ri := sw.DefaultRepositoryInfo()
+	job := c.AddJob(ri)
+
+	c.RunOnce()
+
+	timeout := time.NewTimer(1 * time.Second)
+
+	select {
+	case handledJob := <-o.Started:
+		if job.Id != handledJob.Id {
+			t.Errorf("Job expected id %v. Got %v\n", job.Id, handledJob.Id)
+		}
+	case <-timeout.C:
+		t.Errorf("Job was not handled when RunOnce called.\n")
+	}
+}
+
+func TestRemoveJobAddsToCancellingChannel(t *testing.T) {
+	c, _ := setup()
+
+	ri := sw.DefaultRepositoryInfo()
+	job := c.AddJob(ri)
+	if job == nil {
+		t.Fatalf("Could not add job to the queue.")
+	}
+
+	c.RemoveJob(job)
+
+	timeout := time.NewTimer(1 * time.Second)
+
+	select {
+	case queuedJob := <-c.Cancelling:
+		if job.Id != queuedJob.Id {
+			t.Errorf("Job expected id %v. Got %v\n", job.Id, queuedJob.Id)
+		}
+	case <-timeout.C:
+		t.Errorf("Job was not added to the queue.\n")
+	}
+}
+
+func TestRunOnceHandlesJobFromCancellingChannel(t *testing.T) {
+	c, o := setup()
+
+	ri := sw.DefaultRepositoryInfo()
+	job := c.AddJob(ri)
+
+	c.RunOnce()
+	c.RemoveJob(job)
+	c.RunOnce()
+
+	timeout := time.NewTimer(1 * time.Second)
+
+	select {
+	case handledJob := <-o.Stopped:
+		if job.Id != handledJob.Id {
+			t.Errorf("Job expected id %v. Got %v\n", job.Id, handledJob.Id)
+		}
+	case <-timeout.C:
+		t.Errorf("Job was not handled when RunOnce called.\n")
+	}
+}
+
+func TestCallingStopStopsController(t *testing.T) {
+	c, _ := setup()
+
+	if c.QuitFlag != false {
+		t.Fatalf("Expected controller to be running.\n")
+	}
+
+	c.Stop()
+	c.RunOnce()
+
+	if c.QuitFlag != true {
+		t.Fatalf("Expected controller to be stopped.\n")
+	}
+}
